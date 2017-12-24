cacaf1b4a2e262388ee21b9b47c5296f5e648447
tsh <dr.tallin@gmail.com>
2017-12-24 11:03:54 +0200

    stencil for 4.14.2

--- 4_matrix/lab_hammings_code.py
+++ 4_matrix/lab_hammings_code.py
@@ -1,5 +1,6 @@
 from mat import Mat
 from matutil import listlist2mat
+from vecutil import list2vec
 from GF2 import One
 
 # Task 4.14.1 Create instance of Mat representing the generator matrix G
@@ -10,8 +11,10 @@ G = listlist2mat([[One(), 0, One(), One()],
                   [0, 0, One(), 0],
                   [0, One(), 0, 0],
                   [One(), 0, 0, 0]])
-
-print(G)
+# Task 4.14.2 what is the encoding of the message [1,0,0,1]
+p = list2vec([One(), 0, 0, One()])
+encoded_p = G * p
+print(encoded_p)
 
 
 
