6c46d3743bd412fd6d2220663ab4c3b98cb18cf7
tsh <dr.tallin@gmail.com>
2017-12-24 10:52:51 +0200

    change 1 to GF2 one

--- 4_matrix/lab_hammings_code.py
+++ 4_matrix/lab_hammings_code.py
@@ -3,13 +3,13 @@ from matutil import listlist2mat
 from GF2 import One
 
 # Task 4.14.1 Create instance of Mat representing the generator matrix G
-G = listlist2mat([[1, 0, 1, 1],
-                  [1, 1, 0, 1],
-                  [0, 0, 0, 1],
-                  [1, 1, 1, 0],
-                  [0, 0, 1, 0],
-                  [0, 1, 0, 0],
-                  [1, 0, 0, 0]])
+G = listlist2mat([[One(), 0, One(), One()],
+                  [One(), One(), 0, One()],
+                  [0, 0, 0, One()],
+                  [One(), One(), One(), 0],
+                  [0, 0, One(), 0],
+                  [0, One(), 0, 0],
+                  [One(), 0, 0, 0]])
 
 print(G)
 
