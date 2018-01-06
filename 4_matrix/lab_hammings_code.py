350b63b505dd0ac54214b5a0f1c7090b51f8e5e6
tsh <dr.tallin@gmail.com>
2018-01-07 00:42:36 +0200

    find error procedure

--- 4_matrix/lab_hammings_code.py
+++ 4_matrix/lab_hammings_code.py
@@ -1,6 +1,7 @@
 from mat import Mat
 from matutil import listlist2mat
 from vecutil import list2vec
+from vec import dot
 from GF2 import One
 
 # Task 4.14.1 Create instance of Mat representing the generator matrix G
@@ -15,7 +16,6 @@ G = listlist2mat([[One(), 0, One(), One()],
 # Task 4.14.2 what is the encoding of the message [1,0,0,1]
 p = list2vec([One(), 0, 0, One()])
 encoded_p = G * p
-print(encoded_p)
 
 # Task 4.14.3
 R = listlist2mat([[0, 0, 0, 0, 0, 0, One()],
@@ -28,4 +28,10 @@ H = listlist2mat([[0, 0, 0, One(), One(), One(), One()],
                   [0, One(), One(), 0, 0, One(), One()],
                   [One(), 0, One(), 0, One(), 0, One()]])
 
-
+# Task 4.14.5
+def find_error(e):
+    """ Takes error syndrome and return error vector """
+    pos = dot(e, list2vec([1, 2, 4])) - 1
+    err_vector = [0] * 7
+    err_vector[pos] = 1
+    return list2vec(err_vector)
