4787bdc5008703e42d74a8fafd6ac28df34c240b
tsh <dr.tallin@gmail.com>
2018-01-08 22:31:53 +0200

    identify procedure

--- 4_matrix/lab_transformations_in_2d_geometry.py
+++ 4_matrix/lab_transformations_in_2d_geometry.py
@@ -1,4 +1,10 @@
 from image_mat_util import file2mat, mat2display
+from matutil import identity
+from mat import Mat
 
 # Task 4.15.4
-mat2display(*file2mat('img01.png'))
+# mat2display(*file2mat('img01.png'))
+
+# Task 4.15.5
+def identify():
+    return Mat(({'y', 'u', 'x'}, {'y', 'u', 'x'}), {('x', 'x'): 1, ('y', 'y'): 1, ('u', 'u'): 1})
