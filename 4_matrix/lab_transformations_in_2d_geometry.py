6d38463c1f3297ca21a5e8ccd4c6b81254e8014d
tsh <dr.tallin@gmail.com>
2018-01-09 21:29:03 +0200

    translation

--- 4_matrix/lab_transformations_in_2d_geometry.py
+++ 4_matrix/lab_transformations_in_2d_geometry.py
@@ -1,10 +1,16 @@
 from image_mat_util import file2mat, mat2display
-from matutil import identity
 from mat import Mat
 
 # Task 4.15.4
 # mat2display(*file2mat('img01.png'))
 
 # Task 4.15.5
-def identify():
+def identity():
     return Mat(({'y', 'u', 'x'}, {'y', 'u', 'x'}), {('x', 'x'): 1, ('y', 'y'): 1, ('u', 'u'): 1})
+
+
+# Task 4.15.3
+def translation(alpha, beta):
+    I = identity()
+    I.f.update({('x', 'u'): alpha, ('y', 'u'): beta, ('u', 'u'): 1})
+    return I
