593b54e3b59f6556e19f709a38c03e609fb6d8ab
tsh <dr.tallin@gmail.com>
2018-01-10 22:05:54 +0200

    scale

--- 4_matrix/lab_transformations_in_2d_geometry.py
+++ 4_matrix/lab_transformations_in_2d_geometry.py
@@ -4,6 +4,12 @@ from mat import Mat
 # Task 4.15.4
 # mat2display(*file2mat('img01.png'))
 
+
+def test_mat(m):
+    data, color = file2mat('img01.png')
+    mat2display(m*data, color)
+
+
 # Task 4.15.5
 def identity():
     return Mat(({'y', 'u', 'x'}, {'y', 'u', 'x'}), {('x', 'x'): 1, ('y', 'y'): 1, ('u', 'u'): 1})
@@ -14,3 +20,7 @@ def translation(alpha, beta):
     I = identity()
     I.f.update({('x', 'u'): alpha, ('y', 'u'): beta, ('u', 'u'): 1})
     return I
+
+
+def scale(alpha, beta):
+    return Mat(({'y', 'u', 'x'}, {'y', 'u', 'x'}), {('x', 'x'): alpha, ('y', 'y'): beta, ('u', 'u'): 1})
