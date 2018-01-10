007bc91a4f90f2bcf9f6c6e4391e4eb06bdf1a64
tsh <dr.tallin@gmail.com>
2018-01-10 22:14:14 +0200

    initial rotation

--- 4_matrix/lab_transformations_in_2d_geometry.py
+++ 4_matrix/lab_transformations_in_2d_geometry.py
@@ -1,3 +1,4 @@
+from math import sin, cos
 from image_mat_util import file2mat, mat2display
 from mat import Mat
 
@@ -21,6 +22,18 @@ def translation(alpha, beta):
     I.f.update({('x', 'u'): alpha, ('y', 'u'): beta, ('u', 'u'): 1})
     return I
 
-
+# Task 4.15.4
 def scale(alpha, beta):
     return Mat(({'y', 'u', 'x'}, {'y', 'u', 'x'}), {('x', 'x'): alpha, ('y', 'y'): beta, ('u', 'u'): 1})
+
+
+# Task 14.15.5
+def rotation(theta):
+    return Mat(({'y', 'u', 'x'}, {'y', 'u', 'x'}),
+               {('x', 'x'): cos(theta),
+                ('x', 'y'): sin(theta),
+                ('y', 'x'): -sin(theta),
+                ('y', 'x'): cos(theta),
+                ('u', 'u'): 1})
+
+test_mat(rotation(360))
