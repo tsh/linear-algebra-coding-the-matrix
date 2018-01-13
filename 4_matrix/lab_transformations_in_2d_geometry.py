47b7999eac68c9014883b1a8f0dc0c2792d21004
tsh <dr.tallin@gmail.com>
2018-01-13 16:29:39 +0200

    rm and mv stencil for geomentry lab

--- 4_matrix/lab_transformations_in_2d_geometry.py
+++ 4_matrix/lab_transformations_in_2d_geometry.py
@@ -35,3 +35,73 @@ def rotation(theta):
                 ('y', 'x'): sin(theta),
                 ('y', 'x'): cos(theta),
                 ('u', 'u'): 1})
+
+
+## Task 5
+def rotate_about(theta, x, y):
+    '''
+    Input:  an angle theta (in radians) by which to rotate, and x- and y- coordinates of a point to rotate about
+    Output:  Corresponding 3x3 rotation matrix.
+    It might be helpful to use procedures you already wrote.
+>>> (rotate_about(math.pi/3, 3,4)*Vec({'x','y','u'}, {'x':1, 'y':0, 'u':1}) - Vec({'y', 'x', 'u'},{'y': 0.26794919243112214, 'x': 5.4641016151377535, 'u': 1})).is_almost_zero()
+    True
+    '''
+    pass
+
+## Task 6
+def reflect_y():
+    '''
+    Input:  None.
+    Output:  3x3 Y-reflection matrix.
+
+    >>> reflect_y()*Vec({'x','y','u'}, {'x':1, 'y':1, 'u':1}) == Vec({'x','y','u'}, {'x':-1, 'y':1, 'u':1})
+    True
+    >>> reflect_y()* Vec({'x','y','u'}, {'u':1}) == Vec({'x','y','u'},{'u':1})
+    True
+    '''
+    pass
+
+## Task 7
+def reflect_x():
+    '''
+    Input:  None.
+    Output:  3x3 X-reflection matrix.
+
+    >>> reflect_x()*Vec({'x','y','u'}, {'x':1, 'y':1, 'u':1}) == Vec({'x','y','u'}, {'x':1, 'y':-1, 'u':1})
+    True
+    >>> reflect_x()*Vec({'x','y','u'}, {'u':1}) == Vec({'x','y','u'},{'u':1})
+    True
+    '''
+    pass
+
+## Task 8
+def scale_color(scale_r,scale_g,scale_b):
+    '''
+    Input:  3 scaling parameters for the colors of the image.
+    Output:  Corresponding 3x3 color scaling matrix.
+
+    >>> scale_color(1,2,3)*Vec({'r','g','b'},{'r':1,'g':2,'b':3}) == Vec({'r','g','b'},{'r':1,'g':4,'b':9})
+    True
+    '''
+    pass
+
+## Task 9
+def grayscale():
+    '''
+    Input: None
+    Output: 3x3 greyscale matrix.
+    '''
+    pass
+
+## Task 10
+def reflect_about(x1, y1, x2, y2):
+    '''
+    Input: 2 points that define a line to reflect about.
+    Output:  Corresponding 3x3 reflect about matrix.
+
+    >>> (reflect_about(0,1,1,1) * Vec({'x','y','u'}, {'u':1}) - Vec({'x', 'u', 'y'},{'x': 0.0, 'u': 1, 'y': 2.0})).is_almost_zero()
+    True
+    >>> (reflect_about(0,0,1,1) * Vec({'x','y','u'}, {'x':1, 'u':1}) - Vec({'x', 'u', 'y'},{'u': 1, 'y': 1})).is_almost_zero()
+    True
+    '''
+    pass
